f8f95b3afe22d784af661fbd7cfba0a92f6c5b26
Rafael Katri <rkatri20033@gmail.com>
2022-10-14 13:50:39 -0300

    Dynamixel

--- OpenCR_Side/Servos.h
+++ OpenCR_Side/Servos.h
@@ -1,71 +1,47 @@
-#include <Servo.h>
-#define basearmA_pin 6
-#define basearmB_pin 7
-#define forearm_pin 4
-#define hand_pin 5
-#define gripperTurner_pin 3
-#define gripperOpener_pin 2
-Servo basearmA, basearmB, forearm, hand, gripperTurner, gripperOpener;
+#include <Dynamixel2Arduino.h>
+#define DXL_DIR_PIN 84
+#define basearm_A 0
+#define basearm_B 1
+#define forearm 2
+#define hand 3
+#define gripperOpener 4
+#define gripperTurner 5 
+
+Dynamixel2Arduino dxl(Serial3,DXL_DIR_PIN);
 bool extended = false;
+
+void SetupServo(){
+  dxl.begin(57600);
+}
+
+float constrain(pos, min, max){
+  if (pos < min){
+    pos = min;
+  }
+  if (pos > max){
+    pos = max;
+  }
+  return pos;
+}
 
 void MoveServo(int servo, int pos){
   switch(servo){
     case 0:
-      basearmA.write(constrain(pos, 60, 120));
-      basearmB.write(constrain(180 - pos, 60, 120));
+      dxl.setGoalPosition(basearm_A, constrain(pos, 60, 120), UNIT_DEGREE);
+      dxl.setGoalPosition(basearm_B, constrain(180 - pos, 60, 120), UNIT_DEGREE);
       break;
     case 1:
-      forearm.write(constrain(pos, 20, 120));
+      dxl.setGoalPosition(forearm, constrain(pos, 20, 120), UNIT_DEGREE);
       break;
     case 2:
-      hand.write(constrain(pos, 0, 180));
+      dxl.setGoalPosition(hand, constrain(pos, 0, 180), UNIT_DEGREE);
       break;
     case 3:
-      gripperTurner.write(constrain(pos, 0, 180));
+      dxl.setGoalPosition(gripperOpener, constrain(pos, 0, 180), UNIT_DEGREE);
       break;
     case 4:
-      gripperOpener.write(constrain(pos, 20, 60));
+      dxl.setGoalPosition(gripperTurner, constrain(pos, 20, 60), UNIT_DEGREE);
       break;
   }
 }
 
-void AttachServos(){
-    basearmA.attach(basearmA_pin);
-    basearmB.attach(basearmB_pin);
-    forearm.attach(forearm_pin);
-    hand.attach(hand_pin);
-    gripperOpener.attach(gripperOpener_pin);
-    gripperTurner.attach(gripperTurner_pin);
-}
-
-void DetachServos(){
-    basearmA.detach();
-    basearmB.detach();
-    forearm.detach();
-    hand.detach();
-    gripperOpener.detach();
-    gripperTurner.detach();
-}
-
-void RetractClaw(){
-    forearm.write(0);
-    hand.write(0);
-    basearmA.write(130);
-    basearmB.write(50);
-    gripperOpener.write(60);
-    gripperTurner.write(90);
-    extended = false;
-    delay(3000);
-    DetachServos();
-}
-
-void ExtendClaw(){
-    AttachServos();
-    forearm.write(50);
-    hand.write(180);
-    basearmA.write(70);
-    basearmB.write(110);
-    gripperOpener.write(20);
-    gripperTurner.write(90);
-    extended = true;
-}
