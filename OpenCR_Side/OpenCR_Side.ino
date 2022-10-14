560fa87c71b348f2755c0de6a19765eb682ca780
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-14 13:52:50 -0300

    Update OpenCR_Side.ino

--- OpenCR_Side/OpenCR_Side.ino
+++ OpenCR_Side/OpenCR_Side.ino
@@ -4,6 +4,8 @@
 #include <std_msgs/String.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
 #include "Sensors.h"
 #include "Motors.h"
 #include "Servos.h"
@@ -13,6 +15,9 @@
 String current_command = "";
 int current_value_1 = 0;
 int current_value_2 = 0;
+sensor_msgs::JointState joint_states;
+std_msgs::Float64 position_frontFlipper;
+std_msgs::Float64 position_backFlipper;
 std_msgs::UInt16 gas;
 std_msgs::Float32MultiArray temperature;
 void commandCallback(const std_msgs::String& command){
@@ -24,12 +29,24 @@ void value1Callback(const std_msgs::UInt16& value1){
 void value2Callback(const std_msgs::UInt16& value2){
   current_value_2 = value2.data;
 }
+void jointStateCallback(const sensor_msgs::JointState& joint_state){
+  joint_states = joint_state;
+}
+void FrontFlipperCallback(const std_msgs::Float64& position_frontFlipper){
+  position_frontFlipper = position_frontFlipper;
+}
+void BackFlipperCallback(const std_msgs::Float64& position_backFlipper){
+  position_backFlipper = position_backFlipper;
+}
 ros::NodeHandle nodehandle;
 ros::Publisher pub_temperature("temperature", &temperature);
 ros::Publisher pub_gas("gas", &gas);
 ros::Subscriber<std_msgs::String> sub_command("arduino_command", commandCallback);
 ros::Subscriber<std_msgs::UInt16> sub_value_1("arduino_value_1", value1Callback);
 ros::Subscriber<std_msgs::UInt16> sub_value_2("arduino_value_2", value2Callback);
+ros::Subscriber<sensor_msgs::JointState> sub_joint_state("joint_states", jointStateCallback);
+ros::Subscriber<sensor_msgs::JointState> sub_joint_state_frontFlipper("position_frontFlipper", FrontFlipperCallback);
+ros::Subscriber<sensor_msgs::JointState> sub_joint_state_backFlipper("position_backFlipper", BackFlipperCallback);
 
 void ControlMotors(String command, int command_parameter_1, int command_parameter_2) {
   switch (command)
@@ -78,4 +95,5 @@ void loop() {
   nodehandle.spinOnce();
   delay(1);
   ControlMotors(current_command, current_value_1, current_value_2);
+  ControlDynamixels(joint_states, position_frontFlipper, position_backFlipper);
 }
