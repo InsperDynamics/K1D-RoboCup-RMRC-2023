0e8ad81806d144cccc48f0972b8b351b2c8be825
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2023-06-26 15:29:51 -0300

    Erased things on build folder (and included hazmat model)

--- Main software/build/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googlemock
-# Build directory: /home/borg/K1D-RoboCup-RMRC-2023/Main software/build/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
