86f85d222b30d96e31aebc1026525835289d1531
enricco <riccogemha@gmail.com>
2023-06-27 20:12:26 -0300

    sdl macro mode switch working

---
+++ Main software/build/CMakeFiles/K1D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/K1D.dir/K1D_MAIN.cpp.o"
+  "K1D"
+  "K1D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/K1D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
